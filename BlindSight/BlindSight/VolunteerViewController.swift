8e9450547d1e28678a2fb5e550bd36f8f48f03e5
coder004 <30897961+coder004@users.noreply.github.com>
2019-08-19 23:28:24 -0400

    final edits

---
+++ BlindSight/BlindSight/VolunteerViewController.swift
@@ -0,0 +1,139 @@
+//
+//  VolunteerViewController.swift
+//  BlindSight
+//
+//  Created by Siddharth on 05/05/19.
+//  Copyright © 2019 Siddharth. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+struct MyPlace {
+    var name: String
+    var lat: Double
+    var long: Double
+}
+
+class VolunteerViewController: UIViewController, GMSMapViewDelegate, GMSAutocompleteViewControllerDelegate {
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("ERROR AUTO COMPLETE \(error)")
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+            //self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    let currentLocationMarker = GMSMarker()
+    var locationManager = CLLocationManager()
+    var chosenPlace: MyPlace?
+    
+    let customMarkerWidth: Int = 50
+    let customMarkerHeight: Int = 70
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.startMonitoringSignificantLocationChanges()
+        
+        initGoogleMaps()
+    }
+    
+    func initGoogleMaps() {
+        let camera = GMSCameraPosition.camera(withLatitude: 28.7041, longitude: 77.1025, zoom: 17.0)
+        self.mapView.camera = camera
+        self.mapView.delegate = self
+        self.mapView.isMyLocationEnabled = true
+        self.mapView.delegate = self
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        let lat = place.coordinate.latitude
+        let long = place.coordinate.longitude
+        
+        //showPartyMarkers(lat: lat, long: long)
+        
+        let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: 17.0)
+        mapView.camera = camera
+        chosenPlace = MyPlace(name: place.formattedAddress!, lat: lat, long: long)
+        let marker=GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        marker.title = "\(place.name)"
+        marker.snippet = "\(place.formattedAddress!)"
+        marker.map = mapView
+        
+        self.dismiss(animated: true, completion: nil) // dismiss after place selected
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        guard let customMarkerView = marker.iconView as? CustomMarkerView else { return false }
+        let img = customMarkerView.img!
+        let customMarker = CustomMarkerView(frame: CGRect(x: 0, y: 0, width: customMarkerWidth, height: customMarkerHeight), image: img, borderColor: UIColor.white, tag: customMarkerView.tag)
+        
+        marker.iconView = customMarker
+        
+        return false
+    }
+    
+    func mapView(_ mapView: GMSMapView, markerInfoContents marker: GMSMarker) -> UIView? {
+        guard let customMarkerView = marker.iconView as? CustomMarkerView else { return nil }
+        return nil
+        //let data = previewDemoData[customMarkerView.tag]
+        //restaurantPreviewView.setData(title: data.title, img: data.img, price: data.price)
+       // return restaurantPreviewView
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        guard let customMarkerView = marker.iconView as? CustomMarkerView else { return }
+        let tag = customMarkerView.tag
+       // restaurantTapped(tag: tag)
+    }
+    
+    func mapView(_ mapView: GMSMapView, didCloseInfoWindowOf marker: GMSMarker) {
+        guard let customMarkerView = marker.iconView as? CustomMarkerView else { return }
+        let img = customMarkerView.img!
+        let customMarker = CustomMarkerView(frame: CGRect(x: 0, y: 0, width: customMarkerWidth, height: customMarkerHeight), image: img, borderColor: UIColor.darkGray, tag: customMarkerView.tag)
+        marker.iconView = customMarker
+    }
+    
+}
+
+extension VolunteerViewController: CLLocationManagerDelegate {
+    // 2
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // 3
+        guard status == .authorizedWhenInUse else {
+            return
+        }
+        // 4
+        locationManager.startUpdatingLocation()
+        
+        //5
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+    }
+    
+    // 6
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        
+        // 7
+        mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+        
+        // 8
+        locationManager.stopUpdatingLocation()
+
+    }
+}
+
